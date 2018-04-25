ff94ac23fc149d79698bf16cd6171e8c0cb3d4ee
Martin Sosa <mr.martinsosa@gmail.com>
2018-04-25 12:07:04 -0400

    added enough files to do a Hello World

---
+++ app.rb
@@ -0,0 +1,25 @@
+require "sinatra"
+require "sinatra/reloader"
+require "sendgrid-ruby"
+
+include SendGrid
+
+to = Email.new(email: ENV["PERSONAL_EMAIL"])
+from = Email.new(email: ENV["PERSONAL_EMAIL"])
+
+get "/" do
+    erb :index
+end
+
+post "/catalog" do
+    subject = "Confetti Betty - Product Catalog"
+    content = Content.new(
+        type: 'text/plain', 
+        value: "nothin'"
+    )  
+    mail = Mail.new(from, subject, to, content)
+    sg = SendGrid::API.new(
+        api_key: ENV["SENDGRID_API_KEY"]
+    )
+    response = sg.client.mail._('send').post(request_body: mail.to_json)
+end
